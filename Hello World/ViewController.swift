2370d40ad356a804864fa5dc2217e4a2df0a9b5e
古池勝也 <furuike0816@gmail.com>
2018-12-25 12:52:10 +0900

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by 古池勝也 on 2018/12/25.
+//  Copyright © 2018 katsuya.furuike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
